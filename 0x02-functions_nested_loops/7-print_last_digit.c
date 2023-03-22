89591bd1324dc1ca242c4ba246521f1fcb8fccf7
deltaecho98 <deltaecho1998@gmail.com>
2023-03-22 12:47:30 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+
+
 /**
-* print_last_digit - prints the last digit of a number
-* @n: the int to extract the last digit from
-* Return: value of the last digit
+* main - check the code
+*
+* Return: Always 0.
 */
-int print_last_digit(int n)
+	int main(void)
 {
-int a;	
+	    int r;
 
-if (n < 0)
-n = -n;
 
-a = n % 10;
-
-if (a < 0)
-a = -a;
-
-_putchar(a + '0');
-
-return (a);
-
+	    r = _abs(-1);
+	    printf("%d\n", r);
+	    r = _abs(0);
+	    printf("%d\n", r);
+	    r = _abs(1);
+	    printf("%d\n", r);
+	    r = _abs(-98);
+	    printf("%d\n", r);
+	    return (0);
 }
 
